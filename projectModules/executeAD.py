36c86cf785364562102aff585d6de5849aa11e38
petrusp <florian.petruschke@googlemail.com>
2016-05-04 14:34:30 +0200

    preperation of insert new user to ad

--- projectModules/executeAD.py
+++ projectModules/executeAD.py
@@ -2,9 +2,21 @@ import projectModules.executeSql as executeSql
 import config.adConfig as adConf
 import wmi
 
+# preparing ssh connection to active directory
 def executeCommandOnShellViaSsh(command):
     c=wmi.WMI(adConf.server,user=adConf.username,password=adConf.password)
     process_id, return_value = c.Win32_Process.Create(CommandLine=command)
+
+# insert new user using the preconfigured ssh connection
+def insertNewUser(nameOfNewUser, PhoneNumberOfNewUser):
+    executeCommandOnShellViaSsh("powershell.exe /c "
+                                "Set-ADUser " + nameOfNewUser +
+                                " -OfficePhone " + PhoneNumberOfNewUser)
+
+
+# execute insertion of new user with phone number
+insertNewUser('Ferdi1000', "1234")
+
 
 # module for inserting data into AD
 #def insertUserIntoAD(userId):
@@ -15,7 +27,3 @@ def executeCommandOnShellViaSsh(command):
 #        print(userData)
 
 #insertUserIntoAD(26)
-
-
-executeCommandOnShellViaSsh("powershell.exe /c Set-ADUser Ferdi1 -OfficePhone 1234")
-
