8324a73ff5cafa056107e5d712e1ddbbd7b7a1d1
petrusp <florian.petruschke@googlemail.com>
2016-05-09 11:22:10 +0200

    updated mysql script

--- scripts/setupDbOnLocalhost.sql
+++ scripts/setupDbOnLocalhost.sql
@@ -18,7 +18,7 @@
 -- Execute the following script after being logged in as admin on a MySql-Server            --
 -- ######################################################################################## --
 
-CREATE DATABASE `pythonTest` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `pythonTest` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER 'python'@'localhost' IDENTIFIED BY '***';GRANT USAGE ON *.* TO 'python'@'localhost' IDENTIFIED BY '***' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
 GRANT ALL PRIVILEGES ON `pythonTest`.* TO 'python'@'localhost';
 CREATE TABLE `pythonTest`.`user` ( `user_id` INT NOT NULL AUTO_INCREMENT COMMENT 'Id of the created user' , `name` VARCHAR(255) NOT NULL COMMENT 'name of the user' , `notes` TEXT NULL COMMENT 'notes concerning the user' , PRIMARY KEY (`user_id`)) ENGINE = InnoDB;
